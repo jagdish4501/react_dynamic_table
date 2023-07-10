51ef298887a511f4904d555b8385684cff1a1423
jagdish <pkjagdish4501@gmail.com>
2023-07-10 19:22:25 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     if (dataMap.get(page) !== undefined) {
       setApiData(dataMap.get(page))
     } else
-      axios.get(`url`,
+      axios.get(`https://lumconnectdevproductmanagement.azurewebsites.net/v1/solar/getAllPlants/${pageSize}`,
         {
           headers: {
             'continuationToken': continuationToken
